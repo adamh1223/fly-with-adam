9409a649e41dc12f94c76fcb61eb7171c1c81c08
Adam Hussain <adam@hussmedia.io>
2024-09-26 17:49:31 -0700

    started react tutorial, did set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Home from "./Pages/Home/index";
-import About from "./Pages/About/index";
-import Navbar from "./Pages/Navbar/index";
 
 function App() {
   return (
     <>
       <div className="container">
-        <Navbar />
-        <Home />
-        <About />
+        <h1>yo</h1>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
 //This file is the Vite equivalent of index.js
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import React from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(<App />);
