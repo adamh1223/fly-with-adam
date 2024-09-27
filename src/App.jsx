48fd5b71bed3fbdd3878b46fda51e63116ffc5c1
Adam Hussain <adam@hussmedia.io>
2024-09-26 21:24:57 -0700

    created basic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import React from "react";
+import "./Reset.css";
+import Navbar from "./Pages/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="container">
-        <h1>yo</h1>
-      </div>
-    </>
+    <Router>
+      {/* Router contains all of our pages and paths */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h2>Welcome to the Home Page</h2>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,16 +1,67 @@
-import React, {useState} from 'react'
-
+// Navbar.jsx
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
   return (
     <>
-    <nav className='navbar'>
-        <div className='navbar-container'>
-
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="nav-link">
+            Fly With Adam
+            <i className="fab fa-typo3"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/work" className="nav-links" onClick={closeMobileMenu}>
+                Work
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
-    </nav>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
